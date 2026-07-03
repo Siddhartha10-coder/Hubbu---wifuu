4b9d819b0033cf4909992a3b3a6a827124305d3b
Siddhartha10-coder <konthamsiddhartha5@gmail.com>
2026-07-03 14:28:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-const PASSWORD = "2824";
-
-let pages = [];
 let currentPage = 0;
 
-window.onload = function () {
-    document.getElementById("loading").style.display = "none";
-    document.getElementById("lockscreen").style.display = "block";
+const pages = document.querySelectorAll(".page");
 
-    pages = document.querySelectorAll(".page");
-
-    pages.forEach(page => page.style.display = "none");
-};
+pages.forEach((page, index) => {
+  if (index !== 0) {
+    page.style.display = "none";
+  }
+});
 
 function checkPass() {
-    const input = document.getElementById("password").value;
+  const pass = document.getElementById("password").value;
 
-    if (input === PASSWORD) {
-        document.getElementById("lockscreen").style.display = "none";
-        document.getElementById("book").style.display = "block";
-
-        currentPage = 0;
-        pages[0].style.display = "block";
-    } else {
-        document.getElementById("wrong").innerHTML = "❌ Wrong Passcode";
-    }
+  if (pass === "2824") {
+    document.getElementById("lockscreen").style.display = "none";
+    document.getElementById("book").style.display = "block";
+  } else {
+    document.getElementById("wrong").innerHTML = "❌ Wrong Passcode";
+  }
 }
 
 function nextPage() {
-    pages[currentPage].style.display = "none";
-    currentPage++;
+  pages[currentPage].style.display = "none";
+  currentPage++;
 
-    if (currentPage < pages.length) {
-        pages[currentPage].style.display = "block";
-    }
+  if (currentPage < pages.length) {
+    pages[currentPage].style.display = "block";
+  }
 }
